e4f26e968c0bdb71c2534847ed0d3f7afcd16e74
Amrita0209 <95041559+Amrita0209@users.noreply.github.com>
2022-01-28 18:11:21 +0530

    Add files via upload

---
+++ gravity.py
@@ -0,0 +1,28 @@
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("new.csv")
+mass = df["Mass"].tolist()
+gravity = df["Gravity"].tolist()
+
+X = np.reshape(mass, (len(mass), 1))
+Y = np.reshape(gravity, (len(gravity), 1))
+
+#Code for line plot
+#plt.plot(X, Y, color='red', linewidth=3)
+
+sizes = np.random.uniform(15, 80, len(X))
+colors = np.random.uniform(15, 80, len(X))
+
+fig, ax = plt.subplots()
+ax.scatter(X, Y, s=sizes, c=colors, vmin=0, vmax=100)
+
+plt.ylabel('Y')
+plt.xlabel('X')
+plt.show()
+
+
+
+   
